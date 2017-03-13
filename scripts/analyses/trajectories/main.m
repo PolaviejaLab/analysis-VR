53afb619c27e20a1de3f88bc664671f2a81639ea
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2017-03-13 16:14:38 +0000

    commented the main script of trajectories

--- scripts/analyses/trajectories/main.m
+++ scripts/analyses/trajectories/main.m
@@ -4,7 +4,6 @@
 %
 
 %% Get the data
-
 dataDirectory = 'V:\Data\04. Exp1_Frontiers';
 filesSubjects = dir(fullfile(dataDirectory, 'Subject*'));
 
@@ -15,8 +14,7 @@ f_end = 75;
 cacheDirectory = 'E:\GitHub\analysis-VR\cache';
 files_hands = dir(cacheDirectory);
 
-for i_subject = 1:n_subjects
-    
+for i_subject = 1:n_subjects 
     % subject folder
     subjectFolder = fullfile(dataDirectory, filesSubjects(i_subject).name, '\');
     unityFolder = fullfile(dataDirectory, filesSubjects(i_subject).name, '\Unity_Files\');
@@ -70,67 +68,64 @@ for i_subject = 1:n_subjects
         hands_threat = hands_(start_(1):end_(1), :) * 100;
         
         % put in a struc
-        
-        trajectories{order_dyn(i_t - 3)}.(fileName) = hands_threat;
-        
+        trajectories{order_dyn(i_t - 3)}.(fileName) = hands_threat;  
     end
     
     % plot
-    
     figure, plot(trajectories{1}.(fileName)(1:f_end, 1), trajectories{1}.(fileName)(1:f_end, 3)); hold on
     plot(trajectories{2}.(fileName)(1:f_end, 1), trajectories{2}.(fileName)(1:f_end, 3), 'r')
     plot(trajectories{3}.(fileName)(1:f_end, 1), trajectories{3}.(fileName)(1:f_end, 3), 'g')
     plot(trajectories{4}.(fileName)(1:f_end, 1), trajectories{4}.(fileName)(1:f_end, 3), 'k')
     plot(trajectories{5}.(fileName)(1:f_end, 1), trajectories{5}.(fileName)(1:f_end, 3), 'c')
     xlim([-20 20]); ylim([-13 13])
-       
+    
 end
 
 names = fieldnames(trajectories{1});
 
-figure, 
+figure,
 for i = 1:size(names,1)
     plot(trajectories{2}.(char(names(i)))(1:f_end, 1), trajectories{2}.(char(names(i)))(1:f_end, 3)); hold on
     plot(trajectories{4}.(char(names(i)))(1:f_end, 1), trajectories{4}.(char(names(i)))(1:f_end, 3), 'r');
     plot(trajectories{5}.(char(names(i)))(1:f_end, 1), trajectories{5}.(char(names(i)))(1:f_end, 3), 'k');
 end
 
-figure, 
+figure,
 offset = 00;
 for i = 1:size(names,1)
     plot(trajectories{2}.(char(names(i)))(1:f_end, 1) + offset, trajectories{2}.(char(names(i)))(1:f_end, 3), 'b'); hold on
     plot(trajectories{2}.(char(names(i)))(1, 1) + offset, trajectories{2}.(char(names(i)))(1, 3), 'xb');
-%     plot(trajectories{2}.(char(names(i)))(f_end, 1) + offset, trajectories{2}.(char(names(i)))(f_end, 3), 'xb');
+    %     plot(trajectories{2}.(char(names(i)))(f_end, 1) + offset, trajectories{2}.(char(names(i)))(f_end, 3), 'xb');
     plot(trajectories{3}.(char(names(i)))(1:f_end, 1), trajectories{3}.(char(names(i)))(1:f_end, 3), 'r');
     plot(trajectories{3}.(char(names(i)))(1, 1), trajectories{3}.(char(names(i)))(1, 3), 'xr');
-%     plot(trajectories{3}.(char(names(i)))(f_end, 1), trajectories{3}.(char(names(i)))(f_end, 3), 'xr');
+    %     plot(trajectories{3}.(char(names(i)))(f_end, 1), trajectories{3}.(char(names(i)))(f_end, 3), 'xr');
 end
 
 
-%% velocity
+%% Analysis of velocity
+
+% Get velocity array
 
 v_basal = zeros(74, 19);
 for i = 1:size(names,1)
-x = trajectories{1}.(char(names(i)))(1:f_end, 1);
-y = trajectories{1}.(char(names(i)))(1:f_end, 3);
-v = sqrt((x(2:end)-x(1:end-1)).*(x(2:end)-x(1:end-1)) + (y(2:end)-y(1:end-1)).*(y(2:end)-y(1:end-1)));
-v_basal(:, i) = v;
+    x = trajectories{1}.(char(names(i)))(1:f_end, 1);
+    y = trajectories{1}.(char(names(i)))(1:f_end, 3);
+    v = sqrt((x(2:end)-x(1:end-1)).*(x(2:end)-x(1:end-1)) + (y(2:end)-y(1:end-1)).*(y(2:end)-y(1:end-1)));
+    v_basal(:, i) = v;
 end
 
 
 v_onreal = zeros(74, 19);
 for i = 1:size(names,1)
-x = trajectories{3}.(char(names(i)))(1:f_end, 1);
-y = trajectories{3}.(char(names(i)))(1:f_end, 3);
-v = sqrt((x(2:end)-x(1:end-1)).*(x(2:end)-x(1:end-1)) + (y(2:end)-y(1:end-1)).*(y(2:end)-y(1:end-1)));
-v_onreal(:, i) = v;
+    x = trajectories{3}.(char(names(i)))(1:f_end, 1);
+    y = trajectories{3}.(char(names(i)))(1:f_end, 3);
+    v = sqrt((x(2:end)-x(1:end-1)).*(x(2:end)-x(1:end-1)) + (y(2:end)-y(1:end-1)).*(y(2:end)-y(1:end-1)));
+    v_onreal(:, i) = v;
 end
 
-
-
 % plot mean velocity and std
-figure, hold on, 
-p(1) = plot(nanmean(v_basal, 2), 'linewidth', 2), 
+figure, hold on,
+p(1) = plot(nanmean(v_basal, 2), 'linewidth', 2),
 plot(nanmean(v_basal, 2) + nanstd(v_basal, 1, 2))
 plot(nanmean(v_basal, 2) - nanstd(v_basal, 1, 2))
 p(2) = plot(nanmean(v_onreal, 2), 'linewidth', 2, 'color', 'r'),
@@ -144,48 +139,42 @@ xlabel('frames' );
 title('velocity', 'fontweight', 'bold');
 hold off, box on
 
-
+% Differences in velocity at any timepoint
 addpath('E:\GitHub\analysis-VR\scripts\statistics');
 for kk = 1:74
-    stat_frame(kk)  = stat_bootstrapping(10000, v_basal(kk, :), v_onreal(kk, :));   
+    stat_frame(kk)  = stat_bootstrapping(10000, v_basal(kk, :), v_onreal(kk, :));
 end
 
-
-
-
+% Velocity changes within condition
 m_basal = nanmean(v_basal, 2);
-
 diff_means = nanmean(m_basal([1:34 56:74], :)') - nanmean(m_basal(35:55,:)');
-
 vec_comp = repmat(diff_means, 1, 10000);
 vec_res = zeros(1, 10000);
 
 for i = 1:10000
-    i_rand = randperm(size(m_basal, 2)); 
-    
-        vec_perm = m_basal(1, i_rand);
-        vec_res(i) = nanmean(m_basal([1:34 56:74])) - nanmean(m_basal(35:55));
-    
+    i_rand = randperm(size(m_basal, 2));
+    vec_perm = m_basal(1, i_rand);
+    vec_res(i) = nanmean(m_basal([1:34 56:74])) - nanmean(m_basal(35:55));    
 end
-
 pval_basal = sum(abs(vec_res) > vec_comp, 2)/10000;
 
-
-
 m_onreal = nanmean(v_onreal, 2);
-
 diff_means = nanmean(m_onreal([1:34 56:74], :)') - nanmean(m_onreal(35:55,:)');
-
 vec_comp = repmat(diff_means, 1, 10000);
 vec_res = zeros(1, 10000);
 
 for i = 1:10000
-    i_rand = randperm(size(m_onreal, 2)); 
-    
-        vec_perm = m_onreal(1, i_rand);
-        vec_res(i) = nanmean(m_onreal([1:34 56:74])) - nanmean(m_onreal(35:55));
-    
+    i_rand = randperm(size(m_onreal, 2));
+    vec_perm = m_onreal(1, i_rand);
+    vec_res(i) = nanmean(m_onreal([1:34 56:74])) - nanmean(m_onreal(35:55));
 end
-
 pval_onreal = sum(abs(vec_res) > vec_comp, 2)/10000;
+
+
+
+
+
+
+
+
 

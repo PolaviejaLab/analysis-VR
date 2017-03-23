f6716d61c605d299569c616cf7494cf0f20e4e3a
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2017-03-23 15:37:18 +0000

    deleted unused scripts for this analysis

---
+++ scripts/analyses/analysisGroups.m
@@ -0,0 +1,18 @@
+load('..\..\data\arrays_ownstat.mat');
+load('..\..\data\arrays_owndyn.mat');
+
+addpath('..\statistics\');
+
+% define the groups
+statLow = find(mean(own_stat_nogap) <= 3);
+statMed = find(mean(own_stat_nogap) > 3 & mean(own_stat_nogap) < 5);
+statHig = find(mean(own_stat_nogap) >= 5);
+
+dynLow = find(mean(own_dyn_control) <= 3);
+dynMed = find(mean(own_dyn_control) > 3 & mean(own_dyn_control) < 5);
+dynHig = find(mean(own_dyn_control) >= 5);
+
+% ANALYSIS
+it = 10000;
+
+pval = stat_bygroups(it, statLow, statMed, statHig, own_stat_nogap, own_stat_gap)

9c688ac468eb6e2e30f1287e40d371fb9bbcc23b
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2017-07-27 20:49:15 +0100

    load sociodemographic data

--- scripts/main.m
+++ scripts/main.m
@@ -4,13 +4,18 @@ dataDirectory = 'V:\Data\06. ExpRep';
 
 
 %% Load sociodemographics and other relevant data (sociodemographics.txt)
+% The vectors are already extracted with the subjects that are going to be
+% used for the analysis
 % Adjust the auxiliar variables according to the morphology of the table
-[vecUsed, perFem, meanAge, stdAge, schArray, gameArray, vrArray] = ...
+[vecUsed, perFem, meanAge, stdAge, scholarity, gaming, vr] = ...
     sociodem (dataDirectory);
 
 
 %% Load questionnaire data
 [static_data, dynamic_data] = getData(dataDirectory);
+
+static_data = static_data(:, :, vecUsed);
+dynamic_data = dynamic_data(:, :, vecUsed);
 
 
 %% 

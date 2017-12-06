3bd63ec955426f7622367ad7e5d12ac742273d8b
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2017-12-06 14:32:22 +0000

    GSR changes

--- scripts/analyses/GSR/GSR_main.m
+++ scripts/analyses/GSR/GSR_main.m
@@ -37,38 +37,38 @@ for i_subject = 1:nSubjects
     log_file = dir(fullfile(unityFolder, '*.log'));
     
     % get timestamps
-        [startTS] = getTimestampsFromLog(fullfile(unityFolder, log_file.name),...
-            'Entering state Trial');
-        [endTS] = getTimestampsFromLog(fullfile(unityFolder, log_file.name),...
-            'Event TrialFinished');
+    [startTS] = getTimestampsFromLog(fullfile(unityFolder, log_file.name),...
+        'Entering state Trial');
+    [endTS] = getTimestampsFromLog(fullfile(unityFolder, log_file.name),...
+        'Event TrialFinished');
     [stimulusTS] = getTimestampsFromLog(fullfile(unityFolder, log_file.name), ...
         'Entering state Falling');
     
     % Get Position in the .bin file
-        [i_start] = getIndexBinFile (fullfile(bonsaiFolder, ts_file.name), startTS);
-        [i_end] = getIndexBinFile (fullfile(bonsaiFolder, ts_file.name), endTS);
+    [i_start] = getIndexBinFile (fullfile(bonsaiFolder, ts_file.name), startTS);
+    [i_end] = getIndexBinFile (fullfile(bonsaiFolder, ts_file.name), endTS);
     [i_stimulus] = getIndexBinFile(fullfile(bonsaiFolder, ts_file.name), stimulusTS);
     
-        i_start = unique(i_start); i_end = unique(i_end);
+    i_start = unique(i_start); i_end = unique(i_end);
     i_stimulus = unique(i_stimulus);
-     
+    
     
     
     % Get GSRvalues
     for i_trial = 1:numel(i_stimulus)
         GSR_stim(type_trial_participant(1, i_trial+3)-3).([subjectFolder(i_subject).name]) = ...
             mat_values((i_stimulus(i_trial)-49):(i_stimulus(i_trial)+50));
-    end 
-   
+    end
+    
     
     
     substractedValue = (min(i_start)) -1; % this value will need to be substracted
-%     % so that it does not misaligned now that the mat_values is cut
-%     
-%     % Cut the bin file to adapt it to the end and begging of the experiment
+    %     % so that it does not misaligned now that the mat_values is cut
+    %
+    %     % Cut the bin file to adapt it to the end and begging of the experiment
     mat_values = mat_values(min(i_start):max(i_end));
-%     
-%     % Recalculate all values
+    %
+    %     % Recalculate all values
     i_start = i_start - substractedValue;
     i_end = i_end - substractedValue;
     
@@ -81,17 +81,18 @@ for i_subject = 1:nSubjects
         [i_end] = removeCloseNumbers (i_end);
     end
     
-%     % Separate trials
+    %     % Separate trials
     for i_trial = 1:numel(i_start)
         fprintf('Reading trial %u: ', i_trial);
+        fprintf(' \n');
         try
             GSRstruct(type_trial_participant(1, i_trial)).([subjectFolder(i_subject).name]) = mat_values(i_start(i_trial):i_end(i_trial));
-            fprintf('trial read successfully \n');
+            fprintf('Trial read successfully \n');
         catch
             GSRstruct(type_trial_participant(1, i_trial)).([subjectFolder(i_subject).name]) = [];
-            warning('Failed to read trial');
+            warning('Failed to read trial \n');
         end
     end
     
-% 
+    %
 end

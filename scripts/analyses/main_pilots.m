eb73c0802480c4b8bdd43ac4a382dd1939920afd
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2019-11-07 18:08:41 +0000

    analysis of pilts

--- scripts/analyses/main_pilots.m
+++ scripts/analyses/main_pilots.m
@@ -13,10 +13,8 @@ cOwnership = squeeze(QuestionnaireData(qCOwnership, :, :));
 ownership = ownership'; cOwnership = cOwnership';
 
 [a, b, c] = signrank(ownership(:, 1), cOwnership(:, 1))
-[a, b, c] = signrank(ownership(:, 2), cOwnership(:, 2))
 [a, b, c] = signrank(ownership(:, 3), cOwnership(:, 3))
 [a, b, c] = signrank(ownership(:, 4), cOwnership(:, 4))
-[a, b, c] = signrank(ownership(:, 5), cOwnership(:, 5))
 [a, b, c] = signrank(ownership(:, 6), cOwnership(:, 6))
 
 
@@ -25,7 +23,7 @@ ownership = ownership'; cOwnership = cOwnership';
 [a, b, c] = signrank(ownership(:, 1), ownership(:, 4))
 [a, b, c] = signrank(ownership(:, 3), ownership(:, 6))
 
-p = friedman(ownership, 1);
+[a, b, c] = friedman(ownership(:, [1, 3, 4, 6]), 1);
 
 figure, boxplot(ownership); 
 ylim([0.5 7.5])
@@ -45,10 +43,8 @@ cAgency = squeeze(nanmean(QuestionnaireData(qCAgency, :, :)));
 agency = agency'; cAgency = cAgency';
 
 [a, b, c] = signrank(agency(:, 1), cAgency(:, 1))
-[a, b, c] = signrank(agency(:, 2), cAgency(:, 2))
 [a, b, c] = signrank(agency(:, 3), cAgency(:, 3))
 [a, b, c] = signrank(agency(:, 4), cAgency(:, 4))
-[a, b, c] = signrank(agency(:, 5), cAgency(:, 5))
 [a, b, c] = signrank(agency(:, 6), cAgency(:, 6))
 
 
@@ -57,7 +53,7 @@ agency = agency'; cAgency = cAgency';
 [a, b, c] = signrank(agency(:, 1), agency(:, 4))
 [a, b, c] = signrank(agency(:, 3), agency(:, 6))
 
-p = friedman(agency, 1);
+p = friedman(agency(:, [1, 3, 4, 6]), 1);
 
 figure,
 boxplot(agency); 

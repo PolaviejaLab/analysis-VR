4e87005d080e959b92ece7f40e812b863de903ee
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2020-01-18 11:06:33 +0000

    small changes

--- scripts/analyzeData/questionnaires/statisticsQuestionnaires.m
+++ scripts/analyzeData/questionnaires/statisticsQuestionnaires.m
@@ -2,15 +2,13 @@ function [results] =  statisticsQuestionnaires(data1, data2)
 
 results.SignedRank = signrank (data1, data2);
 
-[R, P] = corrcoef (data1, data2);
-results.pearsonR = R(1,2);
-results.RcorrPval = P(1,2);
+%[R, P] = corrcoef (data1, data2);
+%results.pearsonR = R(1,2);
 
+% [results.stats.spearmanRho, results.RhocorrPval] = ...
+  %  corr(data1', data2', 'Type', 'Spearman');
 
-[results.stats.spearmanRho, results.RhocorrPval] = ...
-    corr(data1', data2', 'Type', 'Spearman');
-
-[results.p_groups, results.n_groups] = ...
-    analysisGroups (data1, data2);
+% [results.p_groups, results.n_groups] = ...
+   % analysisGroups (data1, data2);
 
 end
--- scripts/figures/plotBoxplots.m
+++ scripts/figures/plotBoxplots.m
@@ -6,7 +6,6 @@ n_boxes = size(data, 1);
 
 boxplot(data', 'colors', 'k');
 
-
 title(figure_title);
 ylim([0.5 7.5]);
 
